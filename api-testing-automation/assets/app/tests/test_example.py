5dfc9feaa37eab691059f0bf0ca3f8af7acbc400
laykos0 <laykos0@protonmail.com>
2024-10-11 12:46:00 +0200

    Auto commit on branch main at 2024-10-11 12:46:00

--- api-testing-automation/assets/app/tests/test_example.py
+++ api-testing-automation/assets/app/tests/test_example.py
@@ -6,3 +6,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
+
+def test_read_example():
+    response = client.get("/example")
+    assert response.status_code == 200
