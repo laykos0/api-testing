d192d4463306849ef0ec77e0b8d90e73ae5c4a92
laykos0 <laykos0@protonmail.com>
2024-10-11 12:45:43 +0200

    Auto commit on branch main at 2024-10-11 12:45:43

--- api-testing-automation/assets/app/src/main.py
+++ api-testing-automation/assets/app/src/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/example")
+async def read_example():
+    return 1/0
--- api-testing-automation/steps/pytest/text.md
+++ api-testing-automation/steps/pytest/text.md
@@ -1,5 +1,7 @@
 Show example test
 
+`cat tests/test_example.py`
+
 Run example test
 
 `pytest tests/test_example.py`{{exec}}
